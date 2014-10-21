b09ae9507dbf25335a2205cbc799a85d2d6c5e23
Jonathan Hoy <jonhoy34@gmail.com>
2014-10-20 20:07:20 -0700

    Made ND Correlate a serial CPU implementation

--- Robot_Arm.Tests/Program.cs
+++ Robot_Arm.Tests/Program.cs
@@ -24,16 +24,18 @@ namespace Robot_Arm.Tests
         private static Image<Emgu.CV.Structure.Bgr, byte> Photo;
         static void Main(string[] args)
         {
-            var Y = new double[1000000];
-            var X = new double[1000000,1];
+            var Y = new double[10000000];
+            var X = new double[10000000,3];
             Random rnd = new Random();
             for (int i = 0; i < Y.Length; i++)
             {
                 Y[i] = rnd.NextDouble();
                 X[i, 0] = rnd.NextDouble();
+                X[i, 1] = rnd.NextDouble();
+                X[i, 2] = rnd.NextDouble();
             }
-            var N = new int[]{10000};
-            var R = new double[,]{{0 , 1}};
+            var N = new int[]{10, 10, 10};
+            var R = new double[,]{{0, 1}, {0, 1}, {0, 1}};
             
             
             Stopwatch Timer = new Stopwatch();
@@ -41,36 +43,14 @@ namespace Robot_Arm.Tests
             Console.WriteLine("Video Tests... ");
 
             SpeechDictionary myWords = new SpeechDictionary();
-            try
-            {
-                ImageViewer viewer = new ImageViewer(); //create an image viewer
-                //Capture capture = new Capture(); //create a camera capture
-                //capture.SetCaptureProperty(Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_FRAME_WIDTH, 1280);
-                //capture.SetCaptureProperty(Emgu.CV.CvEnum.CAP_PROP.CV_CAP_PROP_FRAME_HEIGHT, 720);
-                
-                //Thread.Sleep(2000);
-                Application.Idle += new EventHandler(delegate(object sender, EventArgs e)
-                {  //run this until application closed (close button click on image viewer)
 
-                    //Photo = capture.QueryFrame(); //draw the image obtained from camera
-                    Timer.Start();
-                    Statistics DataStats = Robot_Arm.Video.GPU.ND_Correlate(ref Y, ref X, N, R);
-                    //string[] MatchStrings = myWords.GetColorStrings("Red");
-                    //var NewPhoto = Photo.Clone();
-                    //Rectangle myRect = Robot_Arm.Video.ColorObjectRecognizer.GetRegion(MatchStrings, NewPhoto);
-                    Timer.Stop();
-                    //viewer.Image = NewPhoto;
-                    Console.WriteLine("Time to Process Frame {0} ms", Timer.ElapsedMilliseconds);
-                    Timer.Reset();
-                });
-                viewer.ShowDialog();
-                
-                
-
-            }
-            catch (Exception ex)
+            while (true)
             {
-                Console.WriteLine(ex.Message);
+                Timer.Start();
+                Statistics DataStats = Robot_Arm.Video.GPU.ND_Correlate(ref Y, ref X, N, R);
+                Timer.Stop();
+                Console.WriteLine("Time to Process Frame {0} ms", Timer.ElapsedMilliseconds);
+                Timer.Reset();
             }
             
 
