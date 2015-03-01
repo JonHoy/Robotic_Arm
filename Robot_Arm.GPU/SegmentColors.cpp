88c5b474d77148f043df577c599e67fb14f8302b
JonHoy <jonhoy34@gmail.com>
2015-03-01 08:14:52 -0800

    Changed pixel format to a 32bit packed integer that contains RGBA info that is extracted on gpu. Tiled implementation with HSI colorspace. Supports Multiple color points to define a region of a particular color

--- Robot_Arm.GPU/SegmentColors.cpp
+++ Robot_Arm.GPU/SegmentColors.cpp
@@ -1,4 +1,4 @@
-#include <amp.h>
+#include "SegmentColors.h"
 #include "NativeDeclarations.h"
 #include "HSI_Pixel.h"
 #include <assert.h>
@@ -7,116 +7,87 @@ using namespace concurrency;
 
 namespace native_library {
 	namespace details {	
-	template <typename T>
-		void SegmentColors(const T* ImagePtr, int PixelCount, const int Planes, const T* ColorsPtr, int NumColors, int* SelectedColorsPtr)
+		array<int,2> SegmentColors(unsigned char* ImagePtr, int Rows, int Cols, unsigned char* ColorsPtr, int NumColors, const int* SelectionIndex)
 		{
-			static const int TileSize = 256;
-			extent<1> ex(PixelCount);
+			static const int TileSize = 128;
+			extent<2> ex(Rows, Cols);
 			// Create a view over the data on the CPU
-			array_view<const T,2> imageView(PixelCount, Planes, ImagePtr);
-			array_view<const T,2> colorsView(NumColors, Planes, ColorsPtr);
-
-			array_view<int, 1> selectedcolorsView(ex, SelectedColorsPtr);
+			array_view<const int,2> imageView(Rows, Cols, (const int*)ImagePtr);
+			array_view<const int,1> colorsView(NumColors, (const int*)ColorsPtr);
+			array_view<const int> selectionIndexView(NumColors, SelectionIndex);
+			array<int, 2> SelectedColors(ex);
     
 			// Run code on the GPU
-			if (Planes == 3 && NumColors <= TileSize && PixelCount % TileSize == 0) // fast implementation for 3 planes and color count less than or equal to TileSize
+			assert(NumColors <= TileSize); // fast implementation for 3 planes and color count less than or equal to TileSize
+			std::vector<HSI_Pixel> HSIColors(NumColors);
+			for (int i = 0; i < NumColors; i++)
 			{
-				std::vector<HSI_Pixel> HSIColors(NumColors);
-				for (int i = 0; i < NumColors; i++)
-				{
-					float R = (float) colorsView(i, 0);
-					float G = (float) colorsView(i, 1);
-					float B = (float) colorsView(i, 2);
-					HSIColors[i] = RGB2HSI(R, G, B);
-					// If already in HSV
-					//HSIColors[i].H = (float) colorsView(i, 0);
-					//HSIColors[i].S = (float) colorsView(i, 1);
-					//HSIColors[i].I = (float) colorsView(i, 2);
-				}
+				RgbPixel Color;
+				Color.UnpackPixel(colorsView(i));
+				HSIColors[i] = RGB2HSI(Color);
+			}
 				
-				array_view<const HSI_Pixel> HSIColorsView(NumColors, &HSIColors[0]); // make an array view wrapper over the hsi
-
-				parallel_for_each(selectedcolorsView.extent.tile<TileSize>(),  [=] (tiled_index<TileSize> t_idx) restrict(amp)
-				{
-					tile_static HSI_Pixel Color_HSI_Tile[TileSize];
-					tile_static HSI_Pixel ImageTile[TileSize];
-					tile_static int SelectedColorTile[TileSize];
-
-					int iPixel = t_idx.global[0];
-					int LocalIdx = t_idx.local[0];
-					
-					float R = (float) imageView(iPixel, 0);
-					float G = (float) imageView(iPixel, 1);
-					float B = (float) imageView(iPixel, 2);
-					ImageTile[LocalIdx] = RGB2HSI(R, G, B);
-					// If image is already converted to HSV;
-					//ImageTile[LocalIdx].H = (float) imageView(iPixel, 0);
-					//ImageTile[LocalIdx].S = (float) imageView(iPixel, 1);
-					//ImageTile[LocalIdx].I = (float) imageView(iPixel, 2);
-
-					if (LocalIdx < NumColors)
-					{
-						Color_HSI_Tile[LocalIdx] = HSIColorsView(LocalIdx);
-					}
-
-					t_idx.barrier.wait_with_tile_static_memory_fence();
-
-					float min_distance = FLT_MAX;		
-					for (int lColor = 0; lColor < NumColors; lColor++)
-					{
-
-						float distance = HSI_Distance(Color_HSI_Tile[lColor], ImageTile[LocalIdx]);
-						if (min_distance > distance)
-						{
-							min_distance = distance;
-							SelectedColorTile[LocalIdx] = lColor;
-						}
-					}
-					selectedcolorsView(iPixel) = SelectedColorTile[LocalIdx];
-				});
-			}
-			else // C++ AMP SIMPLE MODEL
+			array_view<const HSI_Pixel> HSIColorsView(NumColors, &HSIColors[0]); // make an array view wrapper over the hsi
+			try 
+			{ 	
+				parallel_for_each(SelectedColors.extent.tile<TileSize,1>().pad(),  [=, &SelectedColors] (tiled_index<TileSize,1> t_idx) restrict(amp)
 			{
-				parallel_for_each(selectedcolorsView.extent,  [=] (index<1> idx) restrict(amp)
+				tile_static HSI_Pixel Color_HSI_Tile[TileSize];
+				tile_static HSI_Pixel ImageTile[TileSize];
+				tile_static int SelectedColorTile[TileSize];
+				tile_static int SelectedColorIndexTile[TileSize];
+				bool is_InBounds = t_idx.global[0] < Rows;
+				int LocalIdx = t_idx.local[0];					
+				int Color = 0;
+				if (is_InBounds)
+					Color = imageView(t_idx.global);
+				RgbPixel Pixel;
+				Pixel.UnpackPixel(Color);
+				ImageTile[LocalIdx] = RGB2HSI(Pixel);
+
+				if (LocalIdx < NumColors)
 				{
-					int iPixel = idx[0];
-					float min_distance = FLT_MAX;
-					for (int lColor = 0; lColor < NumColors; lColor++)
+					Color_HSI_Tile[LocalIdx] = HSIColorsView(LocalIdx);
+					SelectedColorIndexTile[LocalIdx] = selectionIndexView(LocalIdx);
+				}
+
+				t_idx.barrier.wait_with_tile_static_memory_fence();
+
+				float min_distance = FLT_MAX;		
+				for (int lColor = 0; lColor < NumColors; lColor++)
+				{
+
+					float distance = HSI_Distance(Color_HSI_Tile[lColor], ImageTile[LocalIdx]);
+					if (min_distance > distance)
 					{
-						float distance = 0;
-
-						for (int kPlane = 0; kPlane < Planes; kPlane++)
-						{
-							float localdist;
-							float localColor = (float) colorsView(lColor, kPlane);
-							float localImage = (float) imageView(iPixel, kPlane);
-							if (localImage > localColor)
-							{
-								localdist = localImage - localColor;
-							}
-							else
-							{
-								localdist = localColor - localImage;		
-							}
-							distance = distance + localdist;
-						}
-						if (min_distance > distance)
-						{
-							min_distance = distance;
-							selectedcolorsView(iPixel) = lColor;
-						}
+						min_distance = distance;
+						SelectedColorTile[LocalIdx] = SelectedColorIndexTile[lColor];
 					}
-				});
+				}
+				if (is_InBounds)
+					SelectedColors(t_idx.global) = SelectedColorTile[LocalIdx];
+			});
 			}
-			// Copy data from GPU to CPU
-			selectedcolorsView.synchronize();
-		}
-	}
+			catch (runtime_exception& ex)
+			{
+				auto message = ex.what();
+				throw ex;
+			}
 
-	void SegmentColorsGPU(const float* Image, int PixelCount, int Planes, const float* Colors, int NumColors, int* SelectedColors) {
-		details::SegmentColors<float>(Image, PixelCount, Planes, Colors, NumColors, SelectedColors);
+			return SelectedColors;
+		}
+		void ConvertToBW(array<int,2>& SelectedColors, int SelectionNumber) {
+			parallel_for_each(SelectedColors.extent, [=, &SelectedColors] (index<2> idx) restrict (amp)
+			{
+				if (SelectedColors(idx) == SelectionNumber)
+				{
+					SelectedColors(idx) = 1;
+				}
+				else
+				{
+					SelectedColors(idx) = 0;	
+				}
+			});
+		} 
 	}
-	void SegmentColorsGPU(const int* Image, int PixelCount, int Planes, const int* Colors, int NumColors, int* SelectedColors) {
-		details::SegmentColors<int>(Image, PixelCount, Planes, Colors, NumColors, SelectedColors);
-	}	
 }
